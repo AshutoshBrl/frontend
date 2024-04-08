e8347c78cfa58689facc27b71191caa9b30373c1
Ashutosh baral <87574557+AshutoshBrl@users.noreply.github.com>
2024-04-08 05:59:27 +0545

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "region" {
 }
 
 variable "tf_backend_bucket_name" {
-  default = "lf-devops-gitops-terraform-state-ash"
+  default = "lf-devops-gitops-terraform-state"
 }
 
 variable "devops_interns_repo_name" {
